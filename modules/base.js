c1781aef5abd0d3d817e2f5d2d403501e68c16a0
Nicolas <nicolas@karmagh.ca>
2025-10-28 11:39:49 -0400

    another typo in paths

--- modules/base.js
+++ modules/base.js
@@ -1,5 +1,5 @@
-import { SystemActor, SystemItem } from "./module/documents.mjs";
-import { VehiculeDataModel, PilotDataModel, TechDataModel } from "./module/data-models.mjs";
+import { SystemActor, SystemItem } from "./modules/documents.mjs";
+import { VehiculeDataModel, PilotDataModel, TechDataModel } from "./modules/data-models.mjs";
 
 Hooks.once("init", () => {
 	// Configure custom Document implementations.
