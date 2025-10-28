4713c43dcb948e989e7a2042b956eb24b90adae1
Nicolas <nicolas@karmagh.ca>
2025-10-28 14:53:59 -0400

    active effects template

--- speedheroes.js
+++ speedheroes.js
@@ -24,7 +24,7 @@ Hooks.once("init", () => {
 	loadTemplates([
 		"systems/speedheroes/templates/actor/parts/actor-equipment.hbs",
 		"systems/speedheroes/templates/actor/parts/actor-pilot.hbs",
-		"systems/speedheroes/templates/actor/parts/actor-effects.hbs"
+		"systems/speedheroes/templates/actor/parts/active-effects.hbs"
 	]);
 
 
